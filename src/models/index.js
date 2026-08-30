a44afbb5458a1df016c065ed34e040b44678aec1
oriafo <abrahamibharunujele@gmail.com>
2026-08-29 20:26:39 -0500

    Fix Sequelize database connection for Kubernetes env vars

--- src/models/index.js
+++ src/models/index.js
@@ -8,7 +8,10 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
     dialect: 'postgres',
+    logging: false,
   },
 );
 
